bfddc0b2e271c15546534195a9b48fe8b636afa6
Nuri <72916917+npuigdemont@users.noreply.github.com>
2022-04-13 20:48:58 +0200

    exercici5

--- vite-project/src/components/Home.vue
+++ vite-project/src/components/Home.vue
@@ -2,16 +2,25 @@
 
 <template>
 <div>
-    <botons @canviaFrase ="canviaFrase"></botons>
+     
+        <h1>Benvingut</h1>
+         <h2>Informació d'utilitat</h2>
+        <p>Clicant al botó de continuar podeu accedir al contingut de la història.</p>
+     <button type="button" class="btn btn-outline-info" v-on:click="isHidden = !isHidden">{{ isHidden === true ? "Mostrar" : "Amaga" }}</button>
+<br>
 
+
+    <botons @canviaFrase ="canviaFrase"></botons>
+<div v-if="!isHidden">
   <Escena :msg="text" v-for="(text, index) in textos"
   :key="index"
   :text="text"
   :class="{active: index === num}"
-  > 
-  </Escena>
-  
+  >  </Escena>
   </div>
+  </div>
+  
+   
   </template>
 
   
@@ -19,12 +28,13 @@
 <script>
     import Botons from './botons.vue';
     import Escena from './Escena.vue';
-  
+    
 
 export default {
     components: {
-        'Escena': Escena,
-        Botons
+        Escena,
+        Botons,
+       
     },
     name: "Home",
     data() {
@@ -36,12 +46,18 @@ export default {
             "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
             "L'heroi va decidir travessar la porta que el portava a casa",
             "Mentrestant, altres herois no van tenir tanta sort en la seva elecció ..."
-            ]
+            ],
+            mostrar: false,
+            isHidden: false
+            
         }
     },
     methods: {
         canviaFrase(data) {
             this.num = data;
+        },
+        toggle(){
+            this.mostrar = !this.mostrar;
         }
     },
     }
@@ -52,6 +68,11 @@ export default {
     .active
 {
   background: rgba(204, 127, 192, 0.95);
+}
+button {
+    margin: 2%;
+    padding: 2%;
+ 
 }
 </style>
   
